683a2226a9f3c7143b3b0dde81a664352cf82742
allelomorph <samuel.pomeroy@atlasschool.com>
2025-04-26 02:05:24 +0000

    first cleanup pass on Settings; disable unused options

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -1,4 +1,4 @@
-#include <unistd.h>  // optarg optind
+#include <unistd.h>  // optarg optind _POSIX_MONOTONIC_CLOCK sysconf
 #include <getopt.h>  // getopt_long
 #include <stdlib.h>  // strtoll exit
 
@@ -29,58 +29,66 @@ void Settings::parseFromArgv(const int argc, char* const* argv) {
         case 'D':
             in_displayname = optarg;
             break;
-        case 'f':
-            out_authfile = optarg;
-            break;
-        case 'F':
-            in_authfile = optarg;
-            break;
-        case 'c':
-            copyauth = true;
-            break;
-        case 'n':
-            copyauth = false;
-            break;
-        case 'W':
-            waitforclient = true;
-            break;
-        case 's':
-            stopwhennone = true;
-            break;
-        case 'k':
-            stopwhennone = false;
-            break;
+        // case 'f':
+        //     out_authfile = optarg;
+        //     break;
+        // case 'F':
+        //     in_authfile = optarg;
+        //     break;
+        // case 'c':
+        //     copyauth = true;
+        //     break;
+        // case 'n':
+        //     copyauth = false;
+        //     break;
+        // case 'W':
+        //     waitforclient = true;
+        //     break;
+        // case 's':
+        //     stopifnoactiveconnx = true;
+        //     break;
+        // case 'k':
+        //     stopifnoactiveconnx = false;
+        //     break;
         case 'e':
             denyallextensions = true;
             break;
         case 'w':
             readwritedebug = true;
             break;
-        case 'm':
-            maxshownlistlen = std::strtoll( optarg, NULL, 0 );
-            break;
-        case 'i':
-            interactive = true;
-            break;
-        case 'b':
-            buffered = true;
-            break;
+        // case 'm':
+        //     maxshownlistlen = std::strtoll( optarg, NULL, 0 );
+        //     break;
+        // case 'i':
+        //     interactive = true;
+        //     break;
+        // case 'b':
+        //     buffered = true;
+        //     break;
         case 'o':
             if ( log_path != nullptr ) {
                 log_ofs.close();
                 std::filesystem::remove( log_path );
-                std::cerr << "Multiple -o options!\n";
-                exit(EXIT_FAILURE);
+                std::cerr << argv[0] << ": -o option may only be used once" << std::endl;
+                exit( EXIT_FAILURE );
             }
-            // if( strcmp(optarg,"-") == 0 )
-            //     out = stdout;
             // TBD consider making log_path into filesystem::path in try/except
             //     and rethrow exceptions indicating file path formatting errors
             log_path = optarg;
+            assert( log_path != nullptr && log_path[0] != '\0' );
+            // getopt_long edge case: parsing an option that takes arguments,
+            //   followed by another option with no whitespace in between, eg:
+            //   " -o--help " (optarg == "--help") or " -f-k " (optarg == "-k")
+            if ( log_path[0] == '-' ) {
+                std::cerr << argv[0] <<
+                    ": file path passed to -o option may not begin with '-' to "
+                    "prevent option parsing errors" << std::endl;
+                exit( EXIT_FAILURE );
+            }
             log_ofs.open( log_path );
             if ( !log_ofs.good() ) {
                 // TBD exception / message
-                exit(EXIT_FAILURE);
+                exit( EXIT_FAILURE );
             }
             // https://stackoverflow.com/q/366955/
             log_os.rdbuf( log_ofs.rdbuf() );
@@ -90,61 +98,71 @@ void Settings::parseFromArgv(const int argc, char* const* argv) {
             case LO_HELP:
                 std::cout << argv[0] <<
                     ": Dump all X protocol data being tunneled from a fake X display to a real one.\n";
-                std::cout << "usage: " << argv[0] <<
-                    " [options] [[--] command args ...]\n"
+                std::cout <<
+                    "usage: " << argv[0] << " [options] [[--] command args ...]\n" <<
                     "--display, -d <display to connect to>\n"
                     "--fakedisplay, -D <display to fake>\n"
-                    "--copyauthentication, -c	Copy credentials\n"
-                    "--nocopyauthentication, -n	Do not copy credentials\n"
-                    "--authfile, -f <file instead of ~/.Xauthority to get credentials from>\n"
-                    "--newauthfile, -F <file instead of ~/.Xauthority to put credentials in>\n"
-                    "--waitforclient, -W		wait for connection even if command terminates\n"
-                    "--stopwhendone, -s		Return when last client disconnects\n"
-                    "--keeprunning, -k		Keep running\n"
+                    // "--copyauthentication, -c	Copy credentials\n"
+                    // "--nocopyauthentication, -n	Do not copy credentials\n"
+                    // "--authfile, -f <file instead of ~/.Xauthority to get credentials from>\n"
+                    // "--newauthfile, -F <file instead of ~/.Xauthority to put credentials in>\n"
+                    // "--waitforclient, -W		wait for connection even if command terminates\n"
+                    // "--stopwhendone, -s		Return when last client disconnects\n"
+                    // "--keeprunning, -k		Keep running\n"
                     "--denyextensions, -e		Fake unavailability of all extensions\n"
                     "--readwritedebug, -w		Print amounts of data read/sent\n"
-                    "--maxlistlength, -m <maximum number of entries in each list shown>\n"
-                    "--outfile, -o <filename>	Output to file instead of stdout\n"
-                    "--buffered, -b			Do not output every line but only when buffer is full\n";
-                exit(EXIT_SUCCESS);
-            case LO_VERSION:
-                // puts(PACKAGE " version " VERSION);
-                exit(EXIT_SUCCESS);
-            case LO_TIMESTAMPS:
-                print_timestamps = true;
-                break;
-            case LO_RELTIMESTAMPS:
-                print_reltimestamps = true;
-                break;
-            case LO_UPTIMESTAMPS:
-// #ifndef HAVE_MONOTONIC_CLOCK
-//                 fprintf(stderr, "--monotonic-timestamps not supported as clock_gettime(MONOTONIC_CLOCK, ) was not available at compile time\n");
-//                 exit(EXIT_FAILURE);
-// #else
-//                 if (sysconf(_SC_MONOTONIC_CLOCK) < 0) {
-//                     fprintf(stderr, "--monotonic-timestamps not supported on this system\n");
-//                     exit(EXIT_FAILURE);
+                    // "--maxlistlength, -m <maximum number of entries in each list shown>\n"
+                    "--outfile, -o <filename>	Output to file instead of stdout\n";
+                    // "--buffered, -b			Do not output every line but only when buffer is full\n";
+                exit( EXIT_SUCCESS );
+            // case LO_VERSION:
+            //     // puts(PACKAGE " version " VERSION);
+            //     exit( EXIT_SUCCESS );
+            // case LO_TIMESTAMPS:
+            //     print_timestamps = true;
+            //     break;
+            // case LO_RELTIMESTAMPS:
+            //     print_reltimestamps = true;
+            //     break;
+//             case LO_UPTIMESTAMPS:
+//                 // Relevant POSIX feature test macro, see:
+//                 //   - https://stackoverflow.com/a/50245503
+//                 //   - man 7 posixoptions
+// #ifndef _POSIX_MONOTONIC_CLOCK
+//                 std::cerr << argv[0] << ": --monotonic-timestamps not supported: "
+//                     "clock_gettime(2) clockid MONOTONIC_CLOCK was not available at compile time" <<
+//                     std::endl;
+//                 exit( EXIT_FAILURE );
+// #elif   _POSIX_MONOTONIC_CLOCK == 0
+//                 // macro defined as 0 necessitates run time option check, see:
+//                 //   - https://pubs.opengroup.org/onlinepubs/9799919799/basedefs/unistd.h.html
+//                 //     "Constants for Options and Option Groups"
+//                 if ( sysconf( _SC_MONOTONIC_CLOCK ) < 0 ) {
+//                     std::cerr << argv[0] << ": --monotonic-timestamps not supported: "
+//                         "clock_gettime(2) clockid MONOTONIC_CLOCK was not available at run time" <<
+//                         std::endl;
+//                     exit( EXIT_FAILURE );
 //                 }
-//                 print_uptimestamps = true;
 // #endif
-                break;
-            case LO_PRINTCOUNTS:
-                print_counts = true;
-                break;
-            case LO_PRINTOFFSETS:
-                print_offsets = true;
-                break;
+//                 print_uptimestamps = true;
+//                 break;
+            // case LO_PRINTCOUNTS:
+            //     print_counts = true;
+            //     break;
+            // case LO_PRINTOFFSETS:
+            //     print_offsets = true;
+            //     break;
             }
             break;
         case ':':
         case '?':
         default:
-            exit(EXIT_FAILURE);
+            exit( EXIT_FAILURE );
         }
     }
 
     if ( optind < argc && argv[optind] != std::string_view( "--" ) ) {
-        cli_subcmd_argc = argc - optind;
-        cli_subcmd_argv = argv + optind;
+        subcmd_argc = argc - optind;
+        subcmd_argv = argv + optind;
     }
 }
--- src/include/Settings.hpp
+++ src/include/Settings.hpp
@@ -27,53 +27,54 @@ private:
     static constexpr struct option _longopts[ _OPTION_CT + 1 ] {
         {"display",              required_argument, NULL,              'd'},
         {"fakedisplay",          required_argument, NULL,              'D'},
-        {"authfile",             required_argument, NULL,              'f'},
-        {"newauthfile",          required_argument, NULL,              'F'},
-        {"copyauthentication",   no_argument,       NULL,              'c'},
-        {"nocopyauthentication", no_argument,       NULL,              'n'},
-        {"waitforclient",        no_argument,       NULL,              'w'},
-        {"stopwhendone",         no_argument,       NULL,              's'},
-        {"keeprunning",          no_argument,       NULL,              'k'},
+        // {"authfile",             required_argument, NULL,              'f'},
+        // {"newauthfile",          required_argument, NULL,              'F'},
+        // {"copyauthentication",   no_argument,       NULL,              'c'},
+        // {"nocopyauthentication", no_argument,       NULL,              'n'},
+        // {"waitforclient",        no_argument,       NULL,              'w'},
+        // {"stopwhendone",         no_argument,       NULL,              's'},
+        // {"keeprunning",          no_argument,       NULL,              'k'},
         {"denyextensions",       no_argument,       NULL,              'e'},
         {"readwritedebug",       no_argument,       NULL,              'w'},
-        {"maxlistlength",        required_argument, NULL,              'm'},
+        // {"maxlistlength",        required_argument, NULL,              'm'},
         {"outfile",              required_argument, NULL,              'o'},
-        {"buffered",             no_argument,       NULL,              'b'},
-        {"interactive",          no_argument,       NULL,              'i'},
+        // {"buffered",             no_argument,       NULL,              'b'},
+        // {"interactive",          no_argument,       NULL,              'i'},
         {"help",                 no_argument,       &_long_only_option, LO_HELP},
         {"version",              no_argument,       &_long_only_option, LO_VERSION},
-        {"timestamps",           no_argument,       &_long_only_option, LO_TIMESTAMPS},
-        {"relative-timestamps",  no_argument,       &_long_only_option, LO_RELTIMESTAMPS},
-        {"monotonic-timestamps", no_argument,       &_long_only_option, LO_UPTIMESTAMPS},
-        {"print-counts",         no_argument,       &_long_only_option, LO_PRINTCOUNTS},
-        {"print-offsets",        no_argument,       &_long_only_option, LO_PRINTOFFSETS},
+        // {"timestamps",           no_argument,       &_long_only_option, LO_TIMESTAMPS},
+        // {"relative-timestamps",  no_argument,       &_long_only_option, LO_RELTIMESTAMPS},
+        // {"monotonic-timestamps", no_argument,       &_long_only_option, LO_UPTIMESTAMPS},
+        // {"print-counts",         no_argument,       &_long_only_option, LO_PRINTCOUNTS},
+        // {"print-offsets",        no_argument,       &_long_only_option, LO_PRINTOFFSETS},
         {NULL,                   0,                 NULL,              0}
     };
-    static constexpr std::string_view _optstring { "+d:D:f:F:cnWskiewm:o:b" };
+    //static constexpr std::string_view _optstring { "+d:D:f:F:cnWskiewm:o:b" };
+    static constexpr std::string_view _optstring { "+d:D:ewo:" };
 
 public:
-    bool readwritedebug          { false };
-    bool copyauth                { true };
-    bool stopwhennone            { true };
-    bool waitforclient           { false };
-    bool denyallextensions       { false };
-    bool interactive             { false };
-    bool print_timestamps        { false };
-    bool print_reltimestamps     { false };
-    bool print_uptimestamps      { false };
-    bool buffered                { false };
-    std::size_t maxshownlistlen  { SIZE_MAX };
-    bool print_counts            { false };
-    bool print_offsets           { false };
-    const char* log_path         { nullptr };
-    std::ofstream log_ofs        {};
-    std::ostream log_os          { std::cout.rdbuf() };
-    const char* out_displayname  { nullptr };
-    const char* in_displayname   { nullptr };
-    const char* out_authfile     { nullptr };
-    const char* in_authfile      { nullptr };
-    char* const* cli_subcmd_argv { nullptr };  // argv after --
-    int cli_subcmd_argc          {};           // cli_subcmd_argv arg ct
+    bool readwritedebug           { false };
+    // bool copyauth                 { true };
+    bool stopifnoactiveconnx      { true };   // stopwhennone
+    bool waitforclient            { false };
+    bool denyallextensions        { false };
+    // bool interactive              { false };
+    // bool print_timestamps         { false };
+    // bool print_reltimestamps      { false };
+    // bool print_uptimestamps       { false };
+    // bool buffered                 { false };
+    // size_t maxshownlistlen        { SIZE_MAX };
+    // bool print_counts             { false };
+    // bool print_offsets            { false };
+    const char*   log_path        { nullptr };
+    std::ofstream log_ofs         {};
+    std::ostream  log_os          { std::cout.rdbuf() };
+    const char*   out_displayname { nullptr };
+    const char*   in_displayname  { nullptr };
+    // const char* out_authfile      { nullptr };
+    // const char* in_authfile       { nullptr };
+    char* const*  subcmd_argv     { nullptr };  // argv after --
+    int           subcmd_argc     {};           // subcmd_argv arg ct
 
     ~Settings();
 
