f32a230eaee6c780b98fa8ebf13b32d27696f568
allelomorph <samuel.pomeroy@atlasschool.com>
2025-09-05 18:44:18 +0000

    ProxyX11Server: break out _pollSingleSocket method

--- src/ProxyX11Server__fetchCurrentServerTime.cpp
+++ src/ProxyX11Server__fetchCurrentServerTime.cpp
@@ -14,25 +14,42 @@
 #include "protocol/errors.hpp"
 
 
-#include <sys/select.h>
-
-
 // no forking or threading, want this to be completed syncronously before main queue begins
 
-// TBD can we be friends with X11ProtocolParser without circular include?
-// "where E is some expression, and pad(E) is the number of bytes needed to
-//   round E up to a multiple of four."
-inline static constexpr size_t _ALIGN { 4 };
-inline static size_t _padToAlignment( const size_t n, const size_t align ) {
-    return n + ( ( align - ( n % align ) ) % align );
-};
-inline static size_t _pad( const size_t n ) {
-    return _padToAlignment( n, _ALIGN );
-}
-inline static size_t _alignedUnits( const size_t sz ) {
-    return sz / _ALIGN;
-}
 
+void ProxyX11Server::_pollSingleSocket(
+    const int socket_fd, const short events ) {
+    assert( events == POLLIN || events == POLLOUT ||
+            events == POLLIN | POLLOUT );
+    const      nfds_t   nfds { 1 };
+    /*struct */pollfd   pfds[1];
+    pfds[0].fd = socket_fd;
+    static constexpr int POLL_TIMEOUT_MS { 3000 };
+
+    pfds[0].events = events;
+    switch ( poll( pfds, nfds, POLL_TIMEOUT_MS ) ) {
+    case 0:
+        throw std::runtime_error(
+            fmt::format( "{}: poll timeout in {} ms",
+                         __PRETTY_FUNCTION__, POLL_TIMEOUT_MS ) );
+        break;
+    case -1:
+        throw errors::system::exception(
+            fmt::format( "{}: poll", __PRETTY_FUNCTION__ ) );
+        break;
+    default:
+        if ( !( pfds[0].revents & events ) ) {
+            throw std::runtime_error(
+                fmt::format(
+                    "{}: poll marked failure from {}",
+                    __PRETTY_FUNCTION__,
+                    pfds[0].revents & POLLERR ? "error condition" :
+                    pfds[0].revents & POLLHUP ? "hangup" :
+                    /*pfds[0].revents & POLLNVAL ?*/ "invalid (closed) fd" ) );
+        }
+        break;
+    }
+}
 
 void ProxyX11Server::_fetchCurrentServerTime() {
     const int server_fd { _connectToServer() };
@@ -43,12 +60,7 @@ void ProxyX11Server::_fetchCurrentServerTime() {
             __PRETTY_FUNCTION__, _out_display.name );
         return;
     }
-
     SocketBuffer sbuffer;
-    const      nfds_t   nfds { 1 };
-    /*struct */pollfd   pfds[1];
-    pfds[0].fd = server_fd;
-    static constexpr int POLL_TIMEOUT_MS { 5000 };
 
     ////// Establish connection // TBD ( could be subroutine to DRY with atom prefetching )
 
@@ -65,212 +77,124 @@ void ProxyX11Server::_fetchCurrentServerTime() {
     sbuffer.load( auth_data, _pad( _AUTH_DATA_SZ ) );
     assert( sbuffer.size() ==
         sizeof(init_header) + _pad( _AUTH_NAME.size() ) + _pad( _AUTH_DATA_SZ ) );
-
-    pfds[0].events = POLLOUT;
-    switch ( poll( pfds, nfds, POLL_TIMEOUT_MS ) ) {
-    case 0:
-        // timeout err ret
-        fmt::println( stderr, "{}: poll timeout on write", __PRETTY_FUNCTION__ );
-        goto close_socket;
-        break;
-    case -1:
-        // errno err ret
-        fmt::println( stderr, "{}: {}",
-                      __PRETTY_FUNCTION__, ::errors::system::message( "poll" ) );
-        goto close_socket;
-        break;
-    default:
-        if ( pfds[0].revents & POLLOUT ) {
-            // TBD try/except?
-            sbuffer.write( server_fd );
-            assert( sbuffer.empty() );
-        } else {
-            // POLLERR/POLLHUP/POLLNVAL err ret
-            fmt::println( stderr, "{}: poll marked failure from {}",
-                          __PRETTY_FUNCTION__,
-                          pfds[0].revents & POLLERR ? "error condition" :
-                          pfds[0].revents & POLLHUP ? "hangup" :
-                          /*pfds[0].revents & POLLNVAL ?*/ "invalid (closed) fd" );
+    {
+        try {
+            _pollSingleSocket( server_fd, POLLOUT );
+        } catch ( const std::exception& e ) {
+            fmt::println( stderr, "{}", e.what() );
             goto close_socket;
         }
-        break;
+        sbuffer.write( server_fd );
+        assert( sbuffer.empty() );
     }
 
     protocol::WINDOW screen0_root;
-    pfds[0].events = POLLIN;
-    switch ( poll(pfds, nfds, POLL_TIMEOUT_MS ) ) {
-    case 0:
-        // timeout err ret
-        fmt::println( stderr, "{}: poll timeout on read", __PRETTY_FUNCTION__ );
-        goto close_socket;
-        break;
-    case -1:
-        // errno err ret
-        break;
-    default:
-        if ( pfds[0].revents & POLLIN ) {
-            sbuffer.read( server_fd );
-            using protocol::connection_setup::ServerAcceptance;
-            assert( sbuffer.size() >= sizeof( ServerAcceptance::Header ) );
-            ServerAcceptance::Header acceptance_header;
-            sbuffer.unload( &acceptance_header, sizeof( ServerAcceptance::Header ) );
-            assert( sbuffer.size() >= _pad( acceptance_header.v ) +
-                    acceptance_header.n * sizeof( ServerAcceptance::FORMAT ) +
-                    sizeof( ServerAcceptance::SCREEN::Header ) );
-            // TBD use err fail instead of assert
-            assert( acceptance_header.success == protocol::connection_setup::SUCCESS );
-            assert( acceptance_header.protocol_major_version ==
-                    init_header.protocol_major_version );
-            assert( acceptance_header.protocol_minor_version ==
-                    init_header.protocol_minor_version );
-            // TBD skip over vendor
-            sbuffer.unload( _pad( acceptance_header.v ) );
-            // TBD skip over pixmap-formats
-            sbuffer.unload( acceptance_header.n * sizeof( ServerAcceptance::FORMAT ) );
-            // TBD get WINDOW for root window of first screen
-            ServerAcceptance::SCREEN::Header screen_header;
-            sbuffer.unload( &screen_header, sizeof( ServerAcceptance::SCREEN::Header ) );
-            screen0_root = screen_header.root;
-            sbuffer.clear();
-        } else {
-            // POLLERR/POLLHUP/POLLNVAL err ret
-            fmt::println( stderr, "{}: poll marked failure from {}",
-                          __PRETTY_FUNCTION__,
-                          pfds[0].revents & POLLERR ? "error condition" :
-                          pfds[0].revents & POLLHUP ? "hangup" :
-                          /*pfds[0].revents & POLLNVAL ?*/ "invalid (closed) fd" );
+    {
+        try {
+            _pollSingleSocket( server_fd, POLLIN );
+        } catch ( const std::exception& e ) {
+            fmt::println( stderr, "{}", e.what() );
             goto close_socket;
         }
-        break;
+        sbuffer.read( server_fd );
+        using protocol::connection_setup::ServerAcceptance;
+        assert( sbuffer.size() >= sizeof( ServerAcceptance::Header ) );
+        ServerAcceptance::Header acceptance_header;
+        sbuffer.unload( &acceptance_header, sizeof( ServerAcceptance::Header ) );
+        assert( sbuffer.size() >= _pad( acceptance_header.v ) +
+                acceptance_header.n * sizeof( ServerAcceptance::FORMAT ) +
+                sizeof( ServerAcceptance::SCREEN::Header ) );
+        // TBD use err fail instead of assert
+        assert( acceptance_header.success == protocol::connection_setup::SUCCESS );
+        assert( acceptance_header.protocol_major_version ==
+                init_header.protocol_major_version );
+        assert( acceptance_header.protocol_minor_version ==
+                init_header.protocol_minor_version );
+        // TBD skip over vendor
+        sbuffer.unload( _pad( acceptance_header.v ) );
+        // TBD skip over pixmap-formats
+        sbuffer.unload( acceptance_header.n * sizeof( ServerAcceptance::FORMAT ) );
+        // TBD get WINDOW for root window of first screen
+        ServerAcceptance::SCREEN::Header screen_header;
+        sbuffer.unload( &screen_header, sizeof( ServerAcceptance::SCREEN::Header ) );
+        screen0_root = screen_header.root;
+        sbuffer.clear();
     }
 
     ////// Send ChangeWindowAttributes on screen->root to toggle reporting PropertNotify events
 
-    pfds[0].events = POLLOUT;
-    switch ( poll(pfds, nfds, POLL_TIMEOUT_MS ) ) {
-    case 0:
-        // timeout err ret
-        fmt::println( stderr, "{}: poll timeout on write", __PRETTY_FUNCTION__ );
-        goto close_socket;
-        break;
-    case -1:
-        // errno err ret
-        fmt::println( stderr, "{}: {}",
-                      __PRETTY_FUNCTION__, ::errors::system::message( "poll" ) );
-        goto close_socket;
-        break;
-    default:
-        if ( pfds[0].revents & POLLOUT ) {
-            using protocol::requests::ChangeWindowAttributes;
-            ChangeWindowAttributes::Encoding encoding {};
-            encoding.opcode = protocol::requests::opcodes::CHANGEWINDOWATTRIBUTES;
-            encoding.request_length = _alignedUnits(
-                sizeof( ChangeWindowAttributes::Encoding ) + sizeof( protocol::VALUE ) );
-            encoding.window = screen0_root;
-            encoding.value_mask = /*XCB_CW_EVENT_MASK*/ 1 << 11;
-            sbuffer.load( &encoding, sizeof( encoding ) );
-            const protocol::VALUE value_list[1] { /*XCB_EVENT_MASK_PROPERTY_CHANGE*/ 1 << 22 };
-            sbuffer.load( value_list, sizeof( protocol::VALUE ) );
-            assert( sbuffer.size() == sizeof( encoding ) + sizeof( protocol::VALUE ) );
-            sbuffer.write( server_fd );
-            assert( sbuffer.empty() );
-        } else {
-            // POLLERR/POLLHUP/POLLNVAL err ret
-             fmt::println( stderr, "{}: poll marked failure from {}",
-                          __PRETTY_FUNCTION__,
-                          pfds[0].revents & POLLERR ? "error condition" :
-                          pfds[0].revents & POLLHUP ? "hangup" :
-                          /*pfds[0].revents & POLLNVAL ?*/ "invalid (closed) fd" );
+    {
+        using protocol::requests::ChangeWindowAttributes;
+        ChangeWindowAttributes::Encoding cwa_encoding {};
+        cwa_encoding.opcode = protocol::requests::opcodes::CHANGEWINDOWATTRIBUTES;
+        cwa_encoding.request_length = _alignedUnits(
+            sizeof( ChangeWindowAttributes::Encoding ) + sizeof( protocol::VALUE ) );
+        cwa_encoding.window = screen0_root;
+        cwa_encoding.value_mask = /*XCB_CW_EVENT_MASK*/ 1 << 11;
+        sbuffer.load( &cwa_encoding, sizeof( cwa_encoding ) );
+        const protocol::VALUE value_list[1] { /*XCB_EVENT_MASK_PROPERTY_CHANGE*/ 1 << 22 };
+        sbuffer.load( value_list, sizeof( protocol::VALUE ) );
+        assert( sbuffer.size() == sizeof( cwa_encoding ) + sizeof( protocol::VALUE ) );
+        try {
+            _pollSingleSocket( server_fd, POLLOUT );
+        } catch ( const std::exception& e ) {
+            fmt::println( stderr, "{}", e.what() );
             goto close_socket;
         }
-        break;
+        sbuffer.write( server_fd );
+        assert( sbuffer.empty() );
     }
 
-    ////// Send ChangeProperty with 0-length append  // TBD need noop that has reply or event with current time
+    ////// Send ChangeProperty with 0-length append as noop
+    // TBD see https://stackoverflow.com/questions/61849695/get-current-x11-server-time
 
-    pfds[0].events = POLLOUT;
-    switch ( poll(pfds, nfds, POLL_TIMEOUT_MS ) ) {
-    case 0:
-        // timeout err ret
-        fmt::println( stderr, "{}: poll timeout on write", __PRETTY_FUNCTION__ );
-        goto close_socket;
-        break;
-    case -1:
-        // errno err ret
-        fmt::println( stderr, "{}: {}",
-                      __PRETTY_FUNCTION__, ::errors::system::message( "poll" ) );
-        goto close_socket;
-        break;
-    default:
-        if ( pfds[0].revents & POLLOUT ) {
-            using protocol::requests::ChangeProperty;
-            ChangeProperty::Encoding encoding {};
-            encoding.opcode = protocol::requests::opcodes::CHANGEPROPERTY;
-            // TBD currenly no enum constants in procotol::, only name arrays
-            encoding.mode = /*XCB_PROP_MODE_APPEND 2*/0x02;
-            encoding.request_length = _alignedUnits( sizeof( encoding ) );
-            encoding.window = screen0_root;
-            encoding.property.data = /*XCB_ATOM_WM_NAME 39*/0x27;
-            encoding.type.data = /*XCB_ATOM_STRING 31*/0x1f;
-            encoding.format = 8;  // (bits per fmt unit)
-            encoding.fmt_unit_ct = 0;  // 0-length append to act as noop
-            sbuffer.load( &encoding, sizeof( encoding ) );
-            assert( sbuffer.size() == sizeof( encoding ) );
-            sbuffer.write( server_fd );
-            assert( sbuffer.empty() );
-        } else {
-            // POLLERR/POLLHUP/POLLNVAL err ret
-            fmt::println( stderr, "{}: poll marked failure from {}",
-                          __PRETTY_FUNCTION__,
-                          pfds[0].revents & POLLERR ? "error condition" :
-                          pfds[0].revents & POLLHUP ? "hangup" :
-                          /*pfds[0].revents & POLLNVAL ?*/ "invalid (closed) fd" );
+    {
+        using protocol::requests::ChangeProperty;
+        ChangeProperty::Encoding cp_encoding {};
+        cp_encoding.opcode = protocol::requests::opcodes::CHANGEPROPERTY;
+        // TBD currenly no enum constants in procotol::, only name arrays
+        cp_encoding.mode = /*XCB_PROP_MODE_APPEND 2*/0x02;
+        cp_encoding.request_length = _alignedUnits( sizeof( cp_encoding ) );
+        cp_encoding.window = screen0_root;
+        cp_encoding.property.data = /*XCB_ATOM_WM_NAME 39*/0x27;
+        cp_encoding.type.data = /*XCB_ATOM_STRING 31*/0x1f;
+        cp_encoding.format = 8;  // (bits per fmt unit)
+        cp_encoding.fmt_unit_ct = 0;  // 0-length append to act as noop
+        sbuffer.load( &cp_encoding, sizeof( cp_encoding ) );
+        assert( sbuffer.size() == sizeof( cp_encoding ) );
+        try {
+            _pollSingleSocket( server_fd, POLLOUT );
+        } catch ( const std::exception& e ) {
+            fmt::println( stderr, "{}", e.what() );
             goto close_socket;
         }
-        break;
+        sbuffer.write( server_fd );
+        assert( sbuffer.empty() );
     }
 
     ////// verify properly formatted PropertyNotify reply, collect timestamp
 
-    pfds[0].events = POLLIN;
-    switch ( poll(pfds, nfds, POLL_TIMEOUT_MS ) ) {
-    case 0:
-        // timeout err ret
-        fmt::println( stderr, "{}: poll timeout on read", __PRETTY_FUNCTION__ );
-        goto close_socket;
-        break;
-    case -1:
-        // errno err ret
-        fmt::println( stderr, "{}: {}",
-                      __PRETTY_FUNCTION__, ::errors::system::message( "poll" ) );
-        goto close_socket;
-        break;
-    default:
-        if ( pfds[0].revents & POLLIN ) {
-            sbuffer.read( server_fd );
-            using protocol::events::PropertyNotify;
-            PropertyNotify::Encoding encoding;
-            assert( sbuffer.size() == sizeof( PropertyNotify::Encoding ) );
-            sbuffer.unload( &encoding, sizeof( encoding ) );
-            assert( encoding.code == protocol::events::codes::PROPERTYNOTIFY );
-            assert( encoding.sequence_number == 2 );  // event from second Request
-            assert( encoding.window.data == screen0_root.data );
-            assert( encoding.atom.data == /*XCB_ATOM_WM_NAME 39*/0x27 );
-            assert( encoding.state == /*XCB_PROPERTY_NEW_VALUE*/0 );
-            // assign reference points
-            settings.ref_TIMESTAMP = encoding.time.data;
-            settings.ref_unix_time = time(nullptr);
-            sbuffer.clear();
-        } else {
-            // POLLERR/POLLHUP/POLLNVAL err ret
-            fmt::println( stderr, "{}: poll marked failure from {}",
-                          __PRETTY_FUNCTION__,
-                          pfds[0].revents & POLLERR ? "error condition" :
-                          pfds[0].revents & POLLHUP ? "hangup" :
-                          /*pfds[0].revents & POLLNVAL ?*/ "invalid (closed) fd" );
+    {
+        try {
+            _pollSingleSocket( server_fd, POLLIN );
+        } catch ( const std::exception& e ) {
+            fmt::println( stderr, "{}", e.what() );
             goto close_socket;
         }
-        break;
+        sbuffer.read( server_fd );
+        using protocol::events::PropertyNotify;
+        PropertyNotify::Encoding pn_encoding;
+        assert( sbuffer.size() == sizeof( PropertyNotify::Encoding ) );
+        sbuffer.unload( &pn_encoding, sizeof( pn_encoding ) );
+        assert( pn_encoding.code == protocol::events::codes::PROPERTYNOTIFY );
+        assert( pn_encoding.sequence_number == 2 );  // event from second Request
+        assert( pn_encoding.window.data == screen0_root.data );
+        assert( pn_encoding.atom.data == /*XCB_ATOM_WM_NAME 39*/0x27 );
+        assert( pn_encoding.state == /*XCB_PROPERTY_NEW_VALUE*/0 );
+        // assign reference points
+        settings.ref_TIMESTAMP = pn_encoding.time.data;
+        settings.ref_unix_time = time(nullptr);
+        sbuffer.clear();
     }
 
 close_socket:
