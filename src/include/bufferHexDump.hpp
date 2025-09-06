62b63317ba2b7eacd6391d44a702b2480a10fe56
allelomorph <samuel.pomeroy@atlasschool.com>
2025-09-06 02:08:15 +0000

    X11ProtocolParser: pop out bufferHexDump to global func

---
+++ src/include/bufferHexDump.hpp
@@ -0,0 +1,12 @@
+#ifndef BUFFERHEXDUMP_HPP
+#define BUFFERHEXDUMP_HPP
+
+#include <string>
+
+#include <cstdint>
+
+
+std::string bufferHexDump( const uint8_t* data, const size_t sz );
+
+
+#endif  // BUFFERHEXDUMP_HPP
