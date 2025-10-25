f938f4fc900d9fbdbfe5fb779b2c39b2d6248440
allelomorph <samuel.pomeroy@atlasschool.com>
2025-10-25 20:19:34 +0000

    SocketBuffer: error messages now use __PRETTY_FUNCTION__

--- src/SocketBuffer.cpp
+++ src/SocketBuffer.cpp
@@ -1,11 +1,12 @@
-#include <string>
-#include <algorithm>  // min
+#include <algorithm>     // min
 
 #include <cassert>
-#include <cstring>  // memcpy
+#include <cstring>       // memcpy
 
 #include <sys/socket.h>  // send recv
 #include <errno.h>
+
+#include <fmt/format.h>
 
 #include "SocketBuffer.hpp"
 #include "errors.hpp"
@@ -21,13 +22,12 @@
 // TBD determine if partial writes are really needed
 size_t SocketBuffer::write( const int sockfd,
                             const size_t bytes_to_write ) {
-    std::string err_stem { "SocketBuffer::write(): " };
     assert( bytes_to_write <= size() );
     ssize_t bytes_written { send(
             sockfd, data(), bytes_to_write, _MSG_NONE ) };
     if ( bytes_written == -1 ) {
         throw errors::system::exception(
-            err_stem + "send" );
+            fmt::format( "{}: {}", __PRETTY_FUNCTION__, "send" ) );
     }
     assert( static_cast< size_t >( bytes_written ) == bytes_to_write );
     // bytes written removed (hidden) from front of buffer
@@ -67,7 +67,6 @@ size_t SocketBuffer::write( const int sockfd ) {
 //   (can we guarantee that the server or client will always alternately flag read/write readiness?)
 size_t SocketBuffer::read( const int sockfd,
                            const size_t bytes_to_read ) {
-    std::string err_stem { "SocketBuffer::read(): " };
     assert( _bytes_available > 0 );
     size_t tl_bytes_read {};
     ssize_t bytes_read {};
@@ -77,7 +76,7 @@ size_t SocketBuffer::read( const int sockfd,
                 _bytes_available, _MSG_NONE );
         if ( bytes_read == -1 ) {
             throw errors::system::exception(
-                err_stem + "recv" );
+                fmt::format( "{}: {}", __PRETTY_FUNCTION__, "recv" ) );
         }
         tl_bytes_read += bytes_read;
         if ( bytes_read == _bytes_available ) {
