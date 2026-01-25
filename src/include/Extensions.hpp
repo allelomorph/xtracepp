c438759dc9b78c5a6b5c74236009f0ea78f64e92
allelomorph <samuel.pomeroy@atlasschool.com>
2026-01-25 20:04:44 +0000

    Extensions: now mapping flags by extension name instead of id

--- src/include/Extensions.hpp
+++ src/include/Extensions.hpp
@@ -17,57 +17,47 @@
  */
 class Extensions {
 private:
-    /** @brief Allows for toggling of extension flags by id rather than
-     *    member name. */
-    std::unordered_map< int, bool& > _flags_by_id {
-        { BIG_REQUESTS, this->big_requests }
+    /** @brief Allows for toggling of extension flags by extension name rather
+     *    than member name. */
+    std::unordered_map< std::string_view, bool& > _flags_by_id {
+        { "BIG-REQUESTS", this->big_requests }
     };
 
 public:
-    /**
-     * @brief Unique extension numbers for compile time identification (as
-     *   opposed to major opcodes assigned by X server at run time).
-     */
-    enum Ids {
-        GENERICEVENT, SHAPE, MIT_SHM, XINPUT, XTEST, BIG_REQUESTS, SYNC,
-        XKEYBOARD, XC_MISC, XFIXES, RENDER, RANDR, XINERAMA, COMPOSITE,
-        DAMAGE, DOUBLE_BUFFER, RECORD, PRESENT, DRI3, X_RESOURCE, XVIDEO,
-        GLX, XFREE86_VIDMODE
-    };
     /** @brief Flag for use of BIG-REQUESTS; when true enables extension
      *    [encoding] and an increase in maximum request size.
      *  [encoding]: https://www.x.org/releases/X11R7.7/doc/bigreqsproto/bigreq.html#Encoding */
     bool big_requests {};
 
     /**
-     * @brief Activate an extension by [id](#Ids).
-     * @param ext_id extension [id](#Ids)
+     * @brief Activate an extension by name.
+     * @param name extension name
      */
     inline void
-    activate( const int ext_id ) {
-        auto it { _flags_by_id.find( ext_id ) };
+    activate( const std::string_view& name ) {
+        auto it { _flags_by_id.find( name ) };
         if ( it != _flags_by_id.end() )
             it->second = true;
     }
     /**
-     * @brief Check by [id](#Ids) if extension is active.
-     * @param ext_id extension [id](#Ids)
+     * @brief Check by name if extension is active.
+     * @param name extension name
      * @return whether extension is active
      */
     inline bool
-    active( const int ext_id ) {
-        auto it { _flags_by_id.find( ext_id ) };
+    active( const std::string_view& name ) {
+        auto it { _flags_by_id.find( name ) };
         if ( it != _flags_by_id.end() )
             return it->second;
         return false;
     }
     /**
-     * @brief Deactivate an extension by [id](#Ids).
-     * @param ext_id extension [id](#Ids)
+     * @brief Deactivate an extension by name.
+     * @param name extension name
      */
     inline void
-    deactivate( const int ext_id ) {
-        auto it { _flags_by_id.find( ext_id ) };
+    deactivate( const std::string_view& name ) {
+        auto it { _flags_by_id.find( name ) };
         if ( it != _flags_by_id.end() )
             it->second = false;
     }
